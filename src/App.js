20ea2991e9e8fd8ff56f63fe5f026b642c3d09ed
오하늘 <nebulass@naver.com>
2022-09-29 02:10:33 +0900

    회원가입 폼 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import { Button, Checkbox, FormControlLabel, TextField, Link, Grid, Typography, Avatar } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Box, Container } from '@mui/system';
+
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+    <Container component='main' maxWidth='xs'>
+			<Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component='h1' sx={{mb: 3, fontWeight: 700}}>Sign in</Typography>
+				<TextField margin='normal' label='Email Address' required fullWidth name='email' autoComplete='email' autoFocus  />
+				<TextField label='Password' type='password' required fullWidth name='password' autoComplete='current-password' sx={{ mb: 1 }} />
+				<FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember Me' />
+				<Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 3 }}>
+					Log in
+				</Button>
+				<Grid container sx={{ pt: '10px' }}>
+					<Grid item xs>
+						<Link>Forgot password?</Link>
+					</Grid>
+					<Grid item>
+						<Link>Sign up</Link>
+					</Grid>
+				</Grid>
+			</Box>
+      </Container>
+		</>
 	);
 }
 
